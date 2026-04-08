1c44f12be19c961a4b9b6e8840d848261e75c25e
IGeorj <masyulionis.geor@gmail.com>
2026-04-08 04:57:57 +0300

    Fixed bug with info updating

--- MCOP/EventListeners/ClientListeners.cs
+++ MCOP/EventListeners/ClientListeners.cs
@@ -77,8 +77,7 @@ public sealed class ClientListeners
 
     public async Task GuildMemberUpdatedHandler(DiscordClient client, GuildMemberUpdatedEventArgs e)
     {
-        if (e.AvatarHashAfter != e.AvatarHashBefore || e.MemberBefore.DisplayName != e.MemberAfter.DisplayName)
-            await _guildUserStatsService.UpdateUserInfo(e.Guild.Id, e.Member.Id, e.MemberAfter.DisplayName, e.AvatarHashAfter);
+        await _guildUserStatsService.UpdateUserInfo(e.Guild.Id, e.Member.Id, e.MemberAfter.DisplayName, e.AvatarHashAfter);
     }
 
     public async Task GuildMemberAddedHandler(DiscordClient client, GuildMemberAddedEventArgs e)
